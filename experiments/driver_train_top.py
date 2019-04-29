3b60cdb4deaecb93c3b01c5c391625f1a2c8e30d
unknown <tayssir.doghri@gmail.com>
2019-04-29 10:37:37 -0400

    update driver_train_top.py

--- experiments/driver_train_top.py
+++ experiments/driver_train_top.py
@@ -42,21 +42,24 @@ if __name__ == '__main__':
     torch.manual_seed(args.seed)
 
     train_loader, test_loader = load_mnist()
-    Phi = custom_feature(train_loader, args.batch_size, args.parser_type, fake_img=False)
 
+    #1-get isometry layer
     with open(os.path.join(args.logdir, args.filename), "rb") as file:
         U = pickle.load(file)
-
-    for layer in range(tree_depth):
-        #compute new feature map
-        Phi = generate_new_phi(Phi, U, layer)
-        #update number of local feature vectors for each image
-        iterates = iterates // 2 
 
     tree_depth = int(math.log2(HEIGHT * WIDTH)) 
     iterates = HEIGHT * WIDTH
 
-    #construct new database
+    print('*** Training top tensor ***')
+    #2-compute reduced feature map
+    Phi = custom_feature(train_loader, args.batch_size, args.parser_type, fake_img=False)
+    for layer in range(tree_depth):
+        Phi = generate_new_phi(Phi, U, layer)
+        #update number of local feature vectors for each image
+        iterates = iterates // 2 
+
+    #3-construct new database representing the training data
+    #images
     t1 = Phi[0].shape[0]
     t2 = Phi[0].shape[1]
     X = np.zeros((args.batch_size, t1*t2))
@@ -64,16 +67,46 @@ if __name__ == '__main__':
         #get reduced Phi
         X[i,:] = Phi[i].flatten()
     print(X.shape)
-
-    #fit logistic regression
+    #labels
     y = np.zeros(args.batch_size)
     for batch_idx, (x, target) in enumerate(train_loader):
         if batch_idx == args.batch_size:
             break
         y[batch_idx] = target[0]  
+
+    #4-fit logistic regression
     logreg = LogisticRegression(C=1e5, solver='newton-cg', 
         multi_class='multinomial',max_iter=100000, warm_start=True)
     logreg.fit(X, y)
+
+    #5-get score on training set
     print(logreg.score(X, y))
 
     # TODO: @TAYSSIR: PLEASE ADD YOUR TEST CODE
+    #-------Evaluate the model on the test set
+    print('*** Evaluation on the test set ***')
+    #1-compute the reduced feature map
+    for layer in range(tree_depth):
+        Phi = generate_new_phi(Phi, U, layer)
+        #update number of local feature vectors for each image
+        iterates = iterates // 2 
+
+    #2-construct new database representing the test data
+    Phi = custom_feature(test_loader, args.batch_size, args.parser_type, fake_img=False)
+    n_test = len(test_loader) #number of images in the test set
+    #images
+    t1 = Phi[0].shape[0]
+    t2 = Phi[0].shape[1]
+    print(t1, t2)
+    X = np.zeros((n_test, t1*t2))
+    for i in range(n_test):
+        #get reduced Phi
+        X[i,:] = Phi[i].flatten()
+    print(X.shape)
+    #labels
+    y = np.zeros(n_test)
+    for batch_idx, (x, target) in enumerate(test_loader):
+        y[batch_idx] = target[0]
+
+    #3-get score on test set
+    print(logreg.score(X, y))
