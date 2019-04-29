b8e42bb6e06bce526386d48e4188c9ec41810745
Bhairav Mehta <bhairavmehta95@gmail.com>
2019-04-28 20:05:21 -0400

    mp done

--- driver_ucg.py
+++ driver_ucg.py
@@ -14,79 +14,67 @@ import math
 import argparse
 import json
 import pickle
+from functools import partial
+import multiprocessing
 
 from datasets.mnist_utils import load_mnist
 from feature_vectors import local_feature_vectors, custom_feature
-from ucg import reduced_covariance, generate_new_phi
+from ucg import reduced_covariance, generate_new_phi, precalculate_traces, rho_ij
 
 from _constants import FEATURE_MAP_D, HEIGHT, WIDTH
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Learning Relevant Features (Stoudemire 2018)')
+    parser.add_argument('--logdir', type=str, default='saved-models/', help='default log directory')
     parser.add_argument('--eps', type=float, default=1e-3, help='Truncation epsilon')
     parser.add_argument('--batch-size', type=int, default=512, help='Batch size for MNIST')
     parser.add_argument('--seed', type=int, default=123, help='Seed')
+    parser.add_argument('--nworkers', type=int, default=4, help='Number of multiprocessing workers')
 
     args = parser.parse_args()
+
+    if not os.path.exists(args.logdir):
+        os.makedirs(args.logdir)
 
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
 
     train_loader, _ = load_mnist()
     Phi = custom_feature(train_loader, args.batch_size, fake_img=False)
-    
+
     print('Size of train_loader: {}'.format(len(train_loader)))
     print('Size of initial Phi: {}'.format(Phi.shape))
 
     tree_depth = int(math.log2(HEIGHT * WIDTH)) 
     iterates = HEIGHT * WIDTH
 
-    tree_tensor = dict()
+    manager = multiprocessing.Manager()
+    tree_tensor = manager.dict()
 
-    for layer in range(tree_depth):
-        print('Layer: {}, Iterates: {}'.format(layer, iterates))
+    with multiprocessing.Pool(processes=args.nworkers) as pool:
+        for layer in range(tree_depth):
+            print('Layer: {}, Iterates: {}'.format(layer, iterates))
+            traces = precalculate_traces(Phi)
+            print('Traces shape: {}'.format(traces.shape))
 
-        for i in range(iterates):
-            if i % 2: continue 
-            
-            #compute rho
-            ind1 = i
-            ind2 = i + 1
-            rho = reduced_covariance(Phi, ind1, ind2)
+            pairs = np.array_split(range(iterates), iterates // 2)
+            pool.map(partial(rho_ij, Phi, traces, tree_tensor, layer, args.eps), pairs)
 
-            # Calculate Eigenvalues
-            e_val, U = np.linalg.eigh(rho) # eigenvalues arranged in ascending order
-            e_val, U = np.flip(e_val), np.flip(U, axis=1) # eigenvalues arranged in descending order
-            trace = np.sum(e_val)
+            print(tree_tensor[layer, 0, 1])
 
-            truncation_sum = 0
-            # Gross notation, but makes indexing nicer
-            first_truncated_eigenvalue = 0
+            #compute new feature map
+            Phi = generate_new_phi(Phi, tree_tensor, layer)
+            #update number of local feature vectors for each image
+            iterates = iterates // 2 
 
-            for eig_idx, e in enumerate(e_val):
-                truncation_sum += e
-                first_truncated_eigenvalue += 1
-
-                if (truncation_sum / trace) > (1 - args.eps):
-                    break
-            
-            # truncation
-            truncated_U = U[:, :first_truncated_eigenvalue] # keep first r cols of U
-
-            # store U
-            tree_tensor[layer, ind1, ind2] = truncated_U
-            
-        #compute new feature map
-        Phi = generate_new_phi(Phi, tree_tensor, layer)
-        #update number of local feature vectors for each image
-        iterates = iterates // 2 
-
+    # Write to file
     print(tree_tensor[8,0,1].shape)
-    with open("treeU_max", "wb") as file:
+    with open(os.path.join(args.logdir, "treeU_max"), "wb") as file:
         pickle.dump(tree_tensor, file)
 
-    #read
-    with open("treeU_max", "rb") as file:
+    # Read for testing
+    with open(os.path.join(args.logdir, "treeU_max"), "rb") as file:
         tree=pickle.load(file)
+
     print(type(tree))
     print(tree[8,0,1].shape)
--- ucg.py
+++ ucg.py
@@ -21,6 +21,33 @@ from feature_vectors import local_feature_vectors, custom_feature
 from _constants import FEATURE_MAP_D, HEIGHT, WIDTH
 
 
+def rho_ij(Phi, traces, tree_tensor, layer, eps, indices):
+    ind1, ind2 = indices
+    rho = reduced_covariance(Phi, ind1, ind2, traces)
+
+    # Calculate Eigenvalues
+    e_val, U = np.linalg.eigh(rho) # eigenvalues arranged in ascending order
+    e_val, U = np.flip(e_val), np.flip(U, axis=1) # eigenvalues arranged in descending order
+    trace = np.sum(e_val)
+
+    truncation_sum = 0
+    # Gross notation, but makes indexing nicer
+    first_truncated_eigenvalue = 0
+
+    for eig_idx, e in enumerate(e_val):
+        truncation_sum += e
+        first_truncated_eigenvalue += 1
+
+        if (truncation_sum / trace) > (1 - eps):
+            break
+    
+    # Truncate U
+    truncated_U = U[:, :first_truncated_eigenvalue] # keep first r cols of U
+
+    # Store U
+    tree_tensor[layer, ind1, ind2] = truncated_U
+
+
 def generate_new_phi(Phi, tree_tensor, layer):
     print('Generating new phi.')
     Phi_new = [] 
@@ -44,11 +71,25 @@ def generate_new_phi(Phi, tree_tensor, layer):
 
         Phi_new.append(np.array(coarse_grained))
         
-    print('New Phi with N {}'.format(len(Phi_new[0])))
+    print('New Phi with N: {}'.format(len(Phi_new[0])))
     return Phi_new
 
 
-def reduced_covariance(Phi, s1, s2):
+def precalculate_traces(Phi):
+    Nt = len(Phi)     #number of images
+    N = len(Phi[0])       #number of local features vectors in Phi
+
+    traces = np.zeros((Nt, N))
+
+    for img_idx in range(Nt):
+        for s in range(N):
+            x = Phi[img_idx][s]
+            traces[img_idx][s] = np.inner(x, x)
+
+    return traces
+
+
+def reduced_covariance(Phi, s1, s2, traces):
     """ Compute the reduced covariance matrix given the two position (s1,s2) in feature matrix of an image.
         Example: to compute the reduced covariance matrix ro34, s1=2 and s2=3"""
     
@@ -67,12 +108,10 @@ def reduced_covariance(Phi, s1, s2):
         phi2 = Phi[img_idx][s2]
         
         #trace over all the indices except s1 and s2
-        # TODO: Parallelize
         trace_tracker = 1
         for s in range(N):
-            if s != s1 and s != s2:
-                x = Phi[img_idx][s]   
-                trace_tracker *= np.inner(x, x)
+            if s != s1 and s != s2:   
+                trace_tracker *= traces[img_idx][s]
                 
         #compute the order 4 tensor
         phi12 = np.outer(phi1, phi2).flatten()
