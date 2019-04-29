801c0043bd161225024a0075e7038f45389237cc
AdelNabli <33068470+AdelNabli@users.noreply.github.com>
2019-04-29 14:05:43 -0400

    added load subset

--- datasets/mnist_utils.py
+++ datasets/mnist_utils.py
@@ -7,6 +7,7 @@ from skimage import io, transform
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
 import torchvision
 from torchvision import transforms, utils
 import random
@@ -15,7 +16,18 @@ import argparse
 import json
 import pickle
 
-def load_mnist(batch_size=1):
+# Return only indices of certain classes
+def get_indices_label(dataset, labels_list):
+    label_indices = []
+
+    for i in range(dataset.__len__()):
+        _, target = dataset.__getitem__(i)
+        if target in labels_list:
+            label_indices.append(i)
+
+    return label_indices
+
+def load_mnist(labels_list, batch_size=1):
     """ Load MNIST dataset"""
 
     #transform input/output to tensor
@@ -24,19 +36,27 @@ def load_mnist(batch_size=1):
         transforms.ToTensor(),  
     ])
 
+
     #train set
     train_set = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    
+    # Get train indices corresponding to the labels_list
+    train_indices = get_indices_label(train_set, labels_list)
     train_loader = torch.utils.data.DataLoader(
                      dataset=train_set,
                      batch_size=batch_size,
+                     sampler=SubsetRandomSampler(train_indices),
                      shuffle=False)
 
     #test set
     test_set = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 
+    # Get test indices corresponding to the labels_list
+    test_indices = get_indices_label(test_set, labels_list)
     test_loader = torch.utils.data.DataLoader(
                     dataset=test_set,
                     batch_size=batch_size,
+                    sampler=SubsetRandomSampler(test_indices),
                     shuffle=False)
 
     return [train_loader, test_loader] 
