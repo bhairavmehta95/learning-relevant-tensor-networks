ace1787ca6b5b6e2fabca546ccada86113bd3bb2
Bhairav Mehta <bhairavmehta95@gmail.com>
2019-05-01 17:38:42 -0400

    more

--- experiments/driver_ucg.py
+++ experiments/driver_ucg.py
@@ -3,9 +3,11 @@
 from __future__ import print_function, division
 import os
 
-os.environ["MKL_NUM_THREADS"] = "10" 
-os.environ["NUMEXPR_NUM_THREADS"] = "10" 
-os.environ["OMP_NUM_THREADS"] = "10" 
+os.environ["OMP_NUM_THREADS"] = "10" # export OMP_NUM_THREADS=4
+os.environ["OPENBLAS_NUM_THREADS"] = "10" # export OPENBLAS_NUM_THREADS=4 
+os.environ["MKL_NUM_THREADS"] = "10" # export MKL_NUM_THREADS=6
+os.environ["VECLIB_MAXIMUM_THREADS"] = "10" # export VECLIB_MAXIMUM_THREADS=4
+os.environ["NUMEXPR_NUM_THREADS"] = "10" # export NUMEXPR_NUM_THREADS=6
 
 import torch
 from skimage import io, transform 
